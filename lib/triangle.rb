12f0708a25eeb76704e099bab2602edc5b052d5c
Steve Anderson <prototype909@msn.com>
2020-05-21 21:01:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       raise TriangleError
     elsif (@side_a + @side_b <= @side_c) || (@side_a + @side_c <= @side_b) || (@side_b + @side_c <= @side_a)
       raise TriangleError
-    else 
+    else
       if (@side_a == @side_b) &&  (@side_b == @side_c)
       :equilateral
       elsif (@side_a != @side_b) && (@side_b != @side_c) && (@side_a != @side_c)
